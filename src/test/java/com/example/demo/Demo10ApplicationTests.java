64d2f8fb6693707368c13216ab81dd7c0fac0284
Rongsen Wu <57533406+wrs-coder@users.noreply.github.com>
2021-11-08 16:41:59 +0800

    update

--- src/test/java/com/example/demo/Demo10ApplicationTests.java
+++ src/test/java/com/example/demo/Demo10ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.Dao.StudentDao;
+import com.example.demo.dao.StudentDao;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -20,8 +18,6 @@ class Demo10ApplicationTests {
 
     @Test
     void contextLoads() throws SQLException {
-//        List<Map<String, Object>> list = studentDao.getStudentList();
-//        System.out.println(list);
 
     }
 }
